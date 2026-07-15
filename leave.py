105b335552be72abd154eb9bd86125ccc77af0b0
Naveen Mandaji <naveenmandaji5760@gmail.com>
2026-07-15 11:33:24 +0530

    Added leave functionality

--- leave.py
+++ leave.py
@@ -1,1 +1,9 @@
 print("Leave Module")
+
+employee_name = input("Enter Employee Name: ")
+leave_days = int(input("Enter Leave Days: "))
+
+if leave_days <= 5:
+    print(employee_name, "Leave Approved")
+else:
+    print(employee_name, "Manager Approval Required")

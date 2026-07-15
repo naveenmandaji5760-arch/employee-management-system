ec1d28c4c6d053d91803792b2ccef59978986294
Naveen Mandaji <naveenmandaji5760@gmail.com>
2026-07-15 20:29:34 +0530

    Added Welcome Admin

--- login.py
+++ login.py
@@ -5,6 +5,5 @@ password = input("Enter Password: ")
 
 if username == "admin" and password == "admin123":
     print("Welcome Admin")
-    print("Login Successful")
 else:
     print("Invalid Credentials")

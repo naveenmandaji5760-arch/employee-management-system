fd7ae4ee1dedb599b1e0cb7d957f6b94ca048d6e
Naveen Mandaji <naveenmandaji5760@gmail.com>
2026-07-15 12:20:28 +0530

    Added leave functionality

--- payroll.py
+++ payroll.py
@@ -1,1 +1,14 @@
 print("Payroll Module")
+
+employee_name = input("Enter Employee Name: ")
+basic_salary = float(input("Enter Basic Salary: "))
+
+hra = basic_salary * 0.20
+da = basic_salary * 0.10
+gross_salary = basic_salary + hra + da
+
+print("Employee Name :", employee_name)
+print("Basic Salary  :", basic_salary)
+print("HRA           :", hra)
+print("DA            :", da)
+print("Gross Salary  :", gross_salary)

d52b9a85f2832d879ef12dd47ef79904d528582b
Naveen Mandaji <naveenmandaji5760@gmail.com>
2026-07-15 20:18:33 +0530

    Add welcome message

--- login.py
+++ login.py
@@ -4,6 +4,7 @@ username = input("Enter Username: ")
 password = input("Enter Password: ")
 
 if username == "admin" and password == "admin123":
+    print("Welcome Admin")
     print("Login Successful")
 else:
     print("Invalid Credentials")
